4851eee7d36d077f367311a3d1f773614562a9fd
andriell <root.andriell@gmail.com>
2019-03-07 17:54:30 +0300

    Start

--- homepage/index/urls.py
+++ homepage/index/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import url
+from django.views.generic import RedirectView
+
 from . import views
 
 urlpatterns = [
-    url('', views.index, name='index'),
+    url(r'^$', views.index, name='index'),
+    url(r'^favicon\.ico$', RedirectView.as_view(url='/static/favicon.ico'), name='favicon'),
 ]
