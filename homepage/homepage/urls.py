419bfd9f5d4ebb7f1c581ec5cc7e6d2af5341d4d
andriell <root.andriell@gmail.com>
2019-03-07 17:12:33 +0300

    Start

--- homepage/homepage/urls.py
+++ homepage/homepage/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('', include('index.urls'), name='index'),
     path('admin/', admin.site.urls),
-    path('webexample/', include('webexample.urls')),
 ]
